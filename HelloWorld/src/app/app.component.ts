121839f6976d91e1da03ef17c112befad026e45d
Lucas <lucasrodriguezy@gmail.com>
2023-09-27 16:55:53 -0300

    estudando

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  template: `<h1>Hello world!</h1>`,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  title = 'Homes';
 }
